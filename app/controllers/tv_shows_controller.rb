f6e400193e4766f60eec8eb9fe66d32176babab2
shivani1106 <shivani.g@techment.com>
2020-02-16 23:30:14 +0530

    added search option

--- app/controllers/tv_shows_controller.rb
+++ app/controllers/tv_shows_controller.rb
@@ -1,7 +1,11 @@
 class TvShowsController < ApplicationController
   
   def index
-    @tv_shows = TvShow.all
+    if params[:search]
+      @tv_shows = TvShow.where("name LIKE ?", "%#{params[:search]}%")&.order("created_at DESC")
+    else
+      @tv_shows = TvShow.all.order('created_at DESC')
+    end
   end
 
   private
