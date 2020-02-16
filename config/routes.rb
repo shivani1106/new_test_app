2867a03d28dcb2e3552f2602cb25ec190cfe3fa5
shivani1106 <shivani.g@techment.com>
2020-02-16 22:33:56 +0530

    add changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,5 @@ Rails.application.routes.draw do
   root to: 'tv_shows#index' 
   devise_for :users
   resources :tv_shows, only: [:index]
-  resources :tv_channels, only: [:index]
-  resources :tv_channels do
-    collection do
-      get 'tv_shows/channel_shows', :to => 'tv_shows#channel_shows'
-    end  
-  end  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
